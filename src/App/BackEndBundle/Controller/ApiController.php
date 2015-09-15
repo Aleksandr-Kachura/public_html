80b1c5461eead820f1e415a9de86e5956cd09e61
valera <valera@example.ru>
2015-09-15 11:34:14 +0300

    AddApi

--- src/App/BackEndBundle/Controller/ApiController.php
+++ src/App/BackEndBundle/Controller/ApiController.php
@@ -4,15 +4,37 @@ namespace App\BackEndBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ApiController extends Controller
 {
-    public function testAction()
+    public function testAction(Request $request)
     {
         header("Access-Control-Allow-Origin: *");
-        $name = "vasia";
-        //$name = json_encode(['firstname'=>$name ]);
-        return new JsonResponse(['firstname'=>$name ]);
+       /* $res = 'Привет';
+        $ar = array("word" => "Привет");
+        header('Content-Type: text/html; charset=utf-8');
+        $en = json_encode($ar, JSON_UNESCAPED_UNICODE);
+
+
+        return new Response($en);
+        return new JsonResponse($en);*/
+
+        $id = $request->get('id');
+        $em = $this->getDoctrine()->getManager();
+
+        $repo = $em->getRepository("BundlesStoreBundle:User2");
+        $user = $repo->getUsid($id);
+        if(!$user)
+        {
+            return new JsonResponse(['error'=>'Fail User']);
+            exit;
+        }
+       // dump($user);
+       // exit;
+        //$name = "vasia";
+       return new JsonResponse($user[0]);
        // return new JsonResponse($name);
     }
 }
